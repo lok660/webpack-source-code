e4497f8258d58d8c3a35593eeb1ca0a7a174c3e4
lok660 <lokk7991@gmail.com>
2022-04-25 21:49:29 +0800

    faet:init demo-project

--- demo-program/build.js
+++ demo-program/build.js
@@ -1,18 +1,21 @@
-const webpack = require('../webpack-master/lib/webpack')
-const config = require('./webpack.config')
+const webpack = require('../webpack-main/lib/webpack');
+const config = require('./webpack.config');
 
-// 执行 webpack 函数有传回调函数
-const compiler = webpack(config, (err, stats) => {
+//  执行webpack函数,有传回调函数
+// const compiler = webpack(config, (err, status) => {
+//   if (err) {
+//     console.log(errr)
+//   }
+// });
+
+
+
+//  执行webpack,不传回调函数
+const compiler = webpack(config);
+//  手动调用run方法
+compiler.run((err, status) => {
   if (err) {
     console.log(err)
   }
 })
 
-// 执行 webpack 函数没有有传回调函数
-// const compiler = webpack(config)
-// // 需要手动调用一下 compiler.run
-// compiler.run((err, stats) => {
-//   if (err) {
-//     console.log(err)
-//   }
-// })
--- demo-program/webpack.config.js
+++ demo-program/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  context: path.resolve(__dirname, "."),
+  context: path.resolve(__dirname, '.'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: "development",
-	devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: ['babel-loader']
       }
     ]
-  },
-  plugins: []
+  }
 }
